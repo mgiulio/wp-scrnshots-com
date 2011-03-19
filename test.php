97cc0281a81a9af161a9d4fe63fef717f18baff4
mgiulio <giulio.mainardi@gmail.com>
2011-03-19 16:01:04 +0100

    Going on...

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
+
 //phpinfo();
 //print_r(ini_get_all());
 //print_r(ini_get_all(null, false));
 //echo ini_get('allow_url_fopen');
 
-$imgurl = 'http://s3.amazonaws.com/scrnshots.com/screenshots/283317/godmotherbackupjpg';
+//$imgurl = 'http://s3.amazonaws.com/scrnshots.com/screenshots/283317/godmotherbackupjpg';
 
 /* if (function_exists('curl_init')) { // Try with Curl 
 	$curl = curl_init();
@@ -28,7 +29,7 @@ $imgurl = 'http://s3.amazonaws.com/scrnshots.com/screenshots/283317/godmotherbac
 	fclose($localimage);
 } */
 
-$full = imagecreatefromjpeg($imgurl);
+/* $full = imagecreatefromjpeg($imgurl);
 //$full = imagecreatefromjpeg('test.jpg');
 if (!$full)
 	echo 'Failed';
@@ -53,7 +54,7 @@ imagecopyresampled($tn, $full, 0, 0, 0, 0, $tnW, $tnH, $w, $h);
 
 imagejpeg($tn, 'tn.jpg');
 imagedestroy($tn);
-imagedestroy($full);
+imagedestroy($full); */
 	
 /*
 // Fetch full size shot from scrnshots.com and copy it to the cache folder
